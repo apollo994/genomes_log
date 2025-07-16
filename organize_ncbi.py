e957c3ef933584e9c84b73986f002254b0edbd8b
apollo994 <fabio.zanarello.94@gmail.com>
2025-07-16 15:36:55 +0200

    make sure permission allow other from group to add folders

--- organize_ncbi.py
+++ organize_ncbi.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 # This script takes a unzipped ncbi datasets folder and a taget location
-# The ncbi folder is copied into the target folder following the 
+# The ncbi folder is copied into the target folder following the
 # species/assembly/file structure. Additionally metadata are saved.
 # Currently the scripts only works with *fna and *gff3 files.
 # An example ncbi comand to get the data is:
 # datasets download genome taxon 2698737 --assembly-level chromosome,complete --annotated --reference  --include genome,gff3
 # the command above downloads all SAR genomes and annotations
-# with the specified carachteristics 
+# with the specified carachteristics
 
 import pandas as pd
 import json
@@ -42,7 +42,7 @@ def main():
     # Iteratively unpack all nested columns
     for column in nested_columns:
         df = unpack_nested_columns(df, column)
-    
+
     # cycle over each assembly in the ncbi_dataset folder
     for assembly_name in df.accession.values:
         tmp_df = df[df.accession == assembly_name]
@@ -52,33 +52,43 @@ def main():
                                                          .replace('/', '_'))
                         + '.'
                         + tmp_df["organism.taxId"].astype(str)).values[0]
-    
+
         # Get the filename without extension
         fna_files = glob(f'{args.ncbi}/data/{assembly_name}/*.fna')
         if not fna_files:
             raise FileNotFoundError(f"No .fna file found in {source_copy}/{assembly_name}")
         filename = os.path.splitext(os.path.basename(fna_files[0]))[0]
-    
+
         # Define source and target paths
         source_genome = f'{args.ncbi}/data/{assembly_name}/*.fna'
         source_ann = f'{args.ncbi}/data/{assembly_name}/*.gff'
         target_folder_path = os.path.join(args.target, species_name, assembly_name)
         target_genome = os.path.join(target_folder_path, f'{filename}.fna')
         target_ann = os.path.join(target_folder_path, f'{filename}.gff')
-    
+
         # Create the target directory if it doesn't exist
         os.makedirs(target_folder_path, exist_ok=True)
-    
+
         # Copy genome and annotation files
         for source_file in glob(source_genome):
             shutil.copy(source_file, target_genome)
         for source_file in glob(source_ann):
             shutil.copy(source_file, target_ann)
-        
+
         # transpose and save assembly metadata
         tmp_df = tmp_df.transpose()
         info_file = f'{args.target}/{species_name}/{assembly_name}/{assembly_name}.info.csv'
         tmp_df.to_csv(info_file, header = False)
+
+        # Chage permission so anyone in the group can modify
+
+        for dirpath, dirnames, filenames in os.walk(f'{args.target}/{species_name}'):
+            os.chmod(dirpath, 0o775)
+            for filename in filenames:
+                filepath = os.path.join(dirpath, filename)
+                os.chmod(filepath, 0o664)  # Set file permissions
+
+        # os.chmod(f'{args.target}/{species_name}', 0o775)
 
         print(species_name, 'genome, annotation and metadata copied!')
 
